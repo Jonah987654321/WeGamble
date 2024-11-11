8dcaa50191c389139f06a4ce84fda6d1ebd7befa
Jonah <82569523+Jonah987654321@users.noreply.github.com>
2024-11-11 10:55:35 +0100

    Fix for fomatCurrency

--- src/modules/utils.php
+++ src/modules/utils.php
@@ -1,11 +1,13 @@
 <?php
 function formatCurrency($balance) {
     $balance = strval($balance);
+    $negative = str_starts_with($balance, "-")?"-":"";
+  	$balance = str_replace("-", "", $balance);
     if (count_chars($balance) > 4) {
         $balance = str_split(strrev($balance), 3);
-        return strrev(join(".", $balance));
+        return $negative.strrev(join(".", $balance));
     } else {
-        return $balance;
+        return $negative.$balance;
     }
 }
 
