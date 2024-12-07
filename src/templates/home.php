ae9eee3193be28b05bec17db48f3de21e8067993
Jonah123456789 <jonah@jo-dev.net>
2024-12-07 22:10:01 +0100

    Home game selector upgrade

--- src/templates/home.php
+++ src/templates/home.php
@@ -36,6 +36,61 @@
                     </div>
                 </div>
             </div>
+            <div class="gameCard">
+                <div class="bgImage">
+                    <img src="https://www.egt.com/wp-content/uploads/2023/07/what-is-a-slot-machine.jpeg">
+                    <div class="gc-Overlay">
+                        <p class="gameName">Slots</p>
+                        <div class="gameButtonWrapper">
+                            <button onclick="window.location.href='/game/blackjack'">Jetzt spielen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gameCard">
+                <div class="bgImage">
+                    <img src="https://www.teachhub.com/wp-content/uploads/2020/05/Classroom-Management-for-an-Effective-Learning-Environment-768x512.jpg">
+                    <div class="gc-Overlay">
+                        <p class="gameName">Hit the Nick</p>
+                        <div class="gameButtonWrapper">
+                            <button onclick="window.location.href='/game/blackjack'">Jetzt spielen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gameCard">
+                <div class="bgImage">
+                    <img src="https://images.t3n.de/news/wp-content/uploads/2023/04/Poker.jpg?class=structuredData-medium">
+                    <div class="gc-Overlay">
+                        <p class="gameName">Poker</p>
+                        <div class="gameButtonWrapper">
+                            <button onclick="window.location.href='/game/blackjack'">Jetzt spielen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gameCard">
+                <div class="bgImage">
+                    <img src="https://media.istockphoto.com/id/1498649784/de/foto/herunterfallen-roter-finanzchart-hintergrund-3d-rendering.jpg?s=612x612&w=0&k=20&c=nw7MFFBDDx-s2M5TGUWO7xUdj1P_CwOCC6OdraMKDEo=">
+                    <div class="gc-Overlay">
+                        <p class="gameName">Crasher</p>
+                        <div class="gameButtonWrapper">
+                            <button onclick="window.location.href='/game/blackjack'">Jetzt spielen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gameCard">
+                <div class="bgImage">
+                    <img src="https://jassverzeichnis.ch/wp-content/uploads/2024/05/Meiern-Titelbild.jpg">
+                    <div class="gc-Overlay">
+                        <p class="gameName">Meiern</p>
+                        <div class="gameButtonWrapper">
+                            <button onclick="window.location.href='/game/blackjack'">Jetzt spielen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
